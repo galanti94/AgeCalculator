47600f1e05f3ae30aa04f1a6308f384b2e499128
Guilherme R Galanti <galanti_imp@hotmail.com>
2022-06-26 18:22:52 -0300

    Remove title bar, adiciona 3 campos

--- app/src/main/java/com/guilhermegalanti/calculadoraminutos/MainActivity.kt
+++ app/src/main/java/com/guilhermegalanti/calculadoraminutos/MainActivity.kt
@@ -2,10 +2,16 @@ package com.guilhermegalanti.calculadoraminutos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Retirar barra de título
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        getSupportActionBar()?.hide()
+
         setContentView(R.layout.activity_main)
     }
 }
