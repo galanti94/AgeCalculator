947b8bd2024ca8640d3d1e992ff7b603513f4f4d
Guilherme R Galanti <galanti_imp@hotmail.com>
2022-06-27 21:41:47 -0300

    design e botão

--- app/src/main/java/com/guilhermegalanti/calculadoraminutos/MainActivity.kt
+++ app/src/main/java/com/guilhermegalanti/calculadoraminutos/MainActivity.kt
@@ -3,6 +3,8 @@ package com.guilhermegalanti.calculadoraminutos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,17 @@ class MainActivity : AppCompatActivity() {
         getSupportActionBar()?.hide()
 
         setContentView(R.layout.activity_main)
+
+
+        //Prgramando botão
+        val btnDate: Button = findViewById(R.id.btn_data)
+        btnDate.setOnClickListener{
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker(){
+        Toast.makeText(this, "Botão pressionado!",Toast.LENGTH_LONG).show()
+
     }
 }
